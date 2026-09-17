50fc2c6e80a7cf84b31ec693ab9273a6bf6d8cef
WebdeveloperNahid <omarfaruk.nahid.webdeveloper@gmail.com>
2026-09-17 23:54:19 +0600

    added a change in white bg them color

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -130,7 +130,7 @@ export default function Skills() {
   return (
     <section
       id="skills"
-      className="relative overflow-hidden bg-white py-24 text-gray-900 dark:bg-[#0A0A0A] dark:text-white lg:py-32"
+      className="relative overflow-hidden bg-[#FAFBFC] py-24 text-gray-900 dark:bg-[#0A0A0A] dark:text-white lg:py-32"
     >
        <GridBackground opacity={0.02} darkOpacity={0.04} size={64} />
       <div className="pointer-events-none absolute left-1/2 top-1/3 h-[500px] w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#2DD3A8]/[0.06] blur-[150px] dark:bg-[#2DD3A8]/5" />
